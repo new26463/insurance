336902a4d9f06fb0d052d218029ab74d656b77c6
new <new26463@gmail.com>
2024-02-19 14:23:57 +0700

    Add (primeng)

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { LangaugeService } from 'src/app/core/services/langauge/langauge.service';
 import { NavbarComponent } from 'src/app/shared/components/navbar';
 
@@ -8,12 +11,14 @@ import { NavbarComponent } from 'src/app/shared/components/navbar';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule, NavbarComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent, InputTextModule, FormsModule, ButtonModule, RouterModule],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
   private readonly _langaugeService: LangaugeService = inject(LangaugeService);
+
+  value!: string;
 
   constructor() {}
 
